26ca4f5f57b641a419a3a8786de61f18cd609b9a
gitsamk <skrame18@gmail.com>
2023-11-30 12:26:52 -0800

    init

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const searchBar = document.getElementById("search");
+const artistName = document.getElementById("artist");
+const container = document.getElementById("container");
+
+
+
+searchBar.addEventListener("click", getEvents); 
+
+
+function getEvents(){
+
+fetch('https://api.predicthq.com/v1/events/?category=concerts', {
+    method: "GET",
+    headers: {
+        "Content-type": "application/json;charset=UTF-8",
+        "Authorization": "Bearer 9EcD8vkazw2_3v-UmcAJ_Zi16l_qrCzyAIWKUD8e"
+      }
+  })
+  .then(response => response.json()) 
+  .then(json => console.log(json.results[0].category))
+  .catch(err => console.log(err));
+
+
+} 
+
+function displayEvents(){
+
+}
+
+
+
+//headers: {"Content-type": "application/json;charset=UTF-8;9EcD8vkazw2_3v-UmcAJ_Zi16l_qrCzyAIWKUD8e"}
