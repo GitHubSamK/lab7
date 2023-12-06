014c65ceefc8976079bcf4e608f98626ec744835
gitsamk <skrame18@gmail.com>
2023-12-06 12:03:20 -0800

    appending results and css

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@ searchBar.addEventListener("click", getEvents);
 
 function getEvents(){
 
-    
+  let artist = artistName.value;
+  container.innerHTML = '';   
 
-fetch('https://api.predicthq.com/v1/events/?category=concerts', {
+fetch('https://api.predicthq.com/v1/events/?category=concerts&q=' + artist, {
     method: "GET",
     headers: {
         "Content-type": "application/json;charset=UTF-8",
@@ -19,16 +20,45 @@ fetch('https://api.predicthq.com/v1/events/?category=concerts', {
       }
   })
   .then(response => response.json()) 
-  .then(json => console.log(json.results))
+  
+  .then(json => {
+    
+    for (let event of json.results) {
+      
+      let concertDiv = document.createElement("div");
+      concertDiv.className = "event";
+      
+      let concertTitle = document.createElement("h2");
+      concertTitle.className = "showtitle";
+      concertTitle.textContent = event.title;
+    
+      let concertLocation = document.createElement("p");
+      concertLocation.className = "showlocation";
+      concertLocation.textContent = event.location[1] + ", " + event.location[0];
+
+      let concertDate = document.createElement("p");
+      concertDate.className = "showdate";
+      concertDate.textContent = event.start;
+     
+     
+      let concertDescription = document.createElement("p");
+      concertDescription.className = "showdescription";
+      concertDescription.textContent = event.description;
+      
+      
+      concertDiv.appendChild(concertTitle);
+      concertDiv.appendChild(concertDescription);
+      concertDiv.appendChild(concertDate);
+      concertDiv.appendChild(concertLocation);
+      
+      container.appendChild(concertDiv);
+    }
+  })
   .catch(err => console.log(err));
-
-
-} 
-
-function displayEvents(){
-
 }
 
 
 
-//headers: {"Content-type": "application/json;charset=UTF-8;9EcD8vkazw2_3v-UmcAJ_Zi16l_qrCzyAIWKUD8e"}
+
+
+
